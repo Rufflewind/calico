8512d90a9601e4d7194d874d386cb3e8cc77d319
Phil Ruffwind <rf@rufflewind.com>
2016-05-18 06:01:19 -0400

    Update tools/profile.py

--- tools/profile.py
+++ tools/profile.py
@@ -17,4 +17,7 @@ for i in range(N):
     d = parse_kvs(subprocess.check_output(["./a.out"]).decode("utf-8"))
     ts.append(float(d["time_random_inserts_1000000_1000000"]))
 
-print(np.mean(ts) * 1e3, "+/-", np.std(ts) * 1e3, "ns")
+tmean = np.mean(ts) * 1e3
+tstd = np.std(ts) * 1e3
+tsdom = tstd / len(ts) ** .5
+print("{0:.1f} +/- {1:.1f} ns; std = {2:.1f}".format(tmean, tsdom, tstd))
